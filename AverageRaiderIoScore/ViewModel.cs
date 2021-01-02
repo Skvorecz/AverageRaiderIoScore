0f4de36553ac81f6efd434e65d2763a746babaa9
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-01-02 22:17:39 +0300

    ViewModel

---
+++ AverageRaiderIoScore/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageRaiderIoScore
+{
+    class ViewModel
+    {
+        List<Character> characters;
+
+        private double GetAverageRaiderIoScore()
+        {
+            return characters.Select(c => c.RaiderIoScore).Average();
+        }
+
+        private double GetAverageItemLvl()
+        {
+            return characters.Select(c => c.ItemLvl).Average();
+        }
+    }
+}
