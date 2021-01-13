5a2400d572d8d62c00c8a32c7c17a242e31b353b
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-01-14 00:10:43 +0300

    Prism connected; Add new char command added

--- AverageRaiderIoScore/ViewModel.cs
+++ AverageRaiderIoScore/ViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using System.Linq;
 using System.Collections.ObjectModel;
+using Prism.Commands;
 
 namespace AverageRaiderIoScore
 {
     class ViewModel
     {
+        public DelegateCommand AddCharacterCommand { get; }
         public string[] Regions { get; } = new string[] { "eu", "us" };
         public ObservableCollection<Character> Characters { get; }
 
         public ViewModel()
         {
-            Characters = new ObservableCollection<Character>() { new Character { Name = "Skvorr", Realm = "Ashenvale", Region = Region.eu },
-                                                                new Character{Name = "Sustamet", Realm = "Gordunni"} };
+            Characters = new ObservableCollection<Character>();
+            AddCharacterCommand = new DelegateCommand(() => AddCharacter());
+        }
+
+        private void AddCharacter()
+        {
+            Characters.Add(new Character());
         }
 
         private double GetAverageRaiderIoScore()
